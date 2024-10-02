e86e4c2bdb78c6242302065da09fae0d2bf8fdbe
Admin <2382039@unai.edu>
2024-10-02 03:33:10 -0800

    Menambahkan

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
-import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class Main {
     public static String[] todos = new String[3];
     public static Scanner scanner = new Scanner(System.in);
 
-    public static main(String [] args) {
-        todos[0] = "Mewarnai";
-        todos[1] = "Membaca";
-        todos[2] = "Bersepeda";
+    public static void main(String [] args) {
+        System.out.println("BEFORE DELETE");
+        addTodoList("Mewarnai");
+        addTodoList("Membaca");
+        addTodoList("Bersepeda");
+        addTodoList("Berkhotbah");
+        showTodoList();
+        editTodoList(3,"Bekerja");
+        System.out.println("AFTER DELETE");
         showTodoList();
     }
 
@@ -16,47 +20,94 @@ public class Main {
         System.out.println("TODO LIST");
         for (int i = 0;i < todos.length; i++){
             String todo = todos[i];
-            if(todo !=null){
-                System.out.println(i+1)+"."+ todo);
+            if(todo !=null) {
+                System.out.println((i + 1) + ". " + todo);
             }
         }
     }
 
     public static void addTodoList(String todo){
-        //resizeArrayIfFull();
-        for (int i=0;i<todos.length;i++){
-            if(todos[i]==null) {
-                todos[i]=todo;
+        resizeArrayIfFull();
+
+        for (int i = 0; i < todos.length; i++){
+            if (todos[i] == null) {
+                todos[i] = todo;
                 break;
             }
         }
     }
 
-    public static void resozeIfFull(){
+    public static void resizeArrayIfFull() {
         //cek whether todos is full
-        Boolean isFull = true;
-        //isFull=isArrayFull(isFull
+        Boolean isFull;
+        isFull = isArrayFull();
 
-        //if full, reaize current arrat to two times bigger
-        if(isFull){
-            //resizeArrayToTwoTimesBigger();
+        //if full, resize current array to two times bigger
+        if (isFull){
+            resizeArrayToTwoTimesBigger();
         }
     }
-}
 
 public static Boolean isArrayFull() {
     for (int i = 0; i < todos.length; i++) {
         if (todos[i] == null) {
             return false;
         }
-        return true;
     }
+    return true;
+}
 
     public static void resizeArrayToTwoTimesBigger() {
         String[] temp = todos;
+        todos = new String[todos.length * 2];
+        for (int i = 0; i < temp.length; i++) {
+            todos[i] = temp[i];
+        }
+    }
 
+    public static boolean removeTodoList(Integer number) {
+        if (isSelectedTodoNotValid(number)) {
+            return false;
+        }
+
+        for (int i = number - 1; i < todos.length; i++ ) {
+            if (i == (todos.length - 1)) {
+                todos[i] = null;
+            } else {
+                //replace with the element on the right
+                todos[i] = todos[i + 1];
+            }
+        }
+        return true;
     }
+
+    public static boolean isSelectedTodoNotValid(Integer number) {
+        //cek if the number is zero or less then zero
+        if (number <= 0) {
+            return true;
+        }
+
+        //check if the number is greater than the todos size/length
+        if (number - 1 > todos.length - 1) {
+            return true;
+        }
+
+        //check whether the element is already null
+        if (todos[number - 1] == null) {
+            return true;
+        }
+
+        return false;
     }
+
+    public static boolean editTodoList(Integer number, String newTodo) {
+        if (isSelectedTodoNotValid(number)) {
+            return false;
+        }
+        todos[number - 1] = newTodo;
+        return true;
+    }
+}
 
 
 
